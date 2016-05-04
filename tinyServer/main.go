a23ca520e099431f069cd3671b6912d537bf2599
edwardrf <edwardrf@gmail.com>
2016-05-04 23:24:30 +0800

    Added README.md, formated existing source code
    Started the archetecture design in README.md
    gofmt the source code

--- tinyServer/main.go
+++ tinyServer/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-    "fmt"
 	"errors"
 	"flag"
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -15,25 +15,25 @@ func main() {
 	flag.StringVar(&carrierProtocol, "carrierProtocol", "udp", "protocol for the underlying layer of tiny vpn.")
 	flag.StringVar(&secret, "secret", "", "secret")
 	flag.StringVar(&serverAddr, "serverAddr", "0.0.0.0:9525", "Server's listening address")
-    flag.StringVar(&vpnnet, "vpnnet", "10.0.200.1/24", "vpn's internal network gateway and netmask.")
+	flag.StringVar(&vpnnet, "vpnnet", "10.0.200.1/24", "vpn's internal network gateway and netmask.")
 	flag.Parse()
 
-    tinyServer, err := CreateTinyServer(secret, carrierProtocol, serverAddr, vpnnet)
+	tinyServer, err := CreateTinyServer(secret, carrierProtocol, serverAddr, vpnnet)
 	defer tinyServer.Close()
 
-    err = tinyServer.Run()
+	err = tinyServer.Run()
 	if err != nil {
-            fmt.Println("Error running tinyServer.")
+		fmt.Println("Error running tinyServer.")
 	}
-    fmt.Println("tinyvpn server started")
+	fmt.Println("tinyvpn server started")
 
-    //Receive system signal to stop the server.
+	//Receive system signal to stop the server.
 	go func() {
 		c := make(chan os.Signal, 1)
 		signal.Notify(c, os.Interrupt, os.Kill, syscall.SIGTERM)
 
 		s := <-c
-        fmt.Println("Received signal",  errors.New(s.String()))
+		fmt.Println("Received signal", errors.New(s.String()))
 	}()
 
 	fmt.Println("process quit")
--- tinyServer/server.go
+++ tinyServer/server.go
@@ -1,155 +1,148 @@
 package main
 
 import (
-    "net"
-    "fmt"
-    "github.com/songgao/water/waterutil"
+	"fmt"
+	"net"
+
+	"github.com/songgao/water/waterutil"
 )
 
 type TinyServer struct {
-    secret string
-    tunnel *Tunnel
-    carrierProtocol string
-    serverAddr string
-    clientRecord map[string]*Client
+	secret          string
+	tunnel          *Tunnel
+	carrierProtocol string
+	serverAddr      string
+	clientMap       map[string]*Client
+	clientList      []*Client
 }
-
 
 //create vpn server, which in turn would create the tunnel interface and bring up it with NAT
 //settings.
 func CreateTinyServer(secret string, carrierProtocol string, serverAddr string, vpnnet string) (*TinyServer, error) {
-    var err error
-    var server TinyServer
+	var err error
+	var server TinyServer
 
-    server.secret = secret
-    server.carrierProtocol = carrierProtocol
-    server.serverAddr = serverAddr
-    server.tunnel, err = CreateTunnel(vpnnet)
-    return &server, err
+	server.secret = secret
+	server.carrierProtocol = carrierProtocol
+	server.serverAddr = serverAddr
+	server.tunnel, err = CreateTunnel(vpnnet)
+	return &server, err
 }
 
 //vpn server starts handling packets.
 func (s *TinyServer) Run() error {
-    var err error
+	var err error
 
-    ct := make(chan[]byte)
-    go s.tunnel.Run(ct)
+	ct := make(chan []byte)
+	go s.tunnel.Run(ct)
 
-    cw := make(chan[]byte)
-    switch s.carrierProtocol {
-        case "udp":
-            go StartUDPServer(s.serverAddr, cw)
-            fmt.Println("Started UDP service.")
-        case "tcp":
-            go StartTCPServer(s.serverAddr, cw)
-            fmt.Println("Started TCP service.")
-        default:
-            fmt.Println("tinyvpn can only run on udp or tcp protocol.")
-    }
+	cw := make(chan []byte)
+	switch s.carrierProtocol {
+	case "udp":
+		go StartUDPServer(s.serverAddr, cw)
+		fmt.Println("Started UDP service.")
+	case "tcp":
+		go StartTCPServer(s.serverAddr, cw)
+		fmt.Println("Started TCP service.")
+	default:
+		fmt.Println("tinyvpn can only run on udp or tcp protocol.")
+	}
 
-    for {
-        select {
-            case tunnelData := <-ct:
-                 err = s.handleTunnelData(tunnelData)
-            case wanData := <-cw:
-                 err = s.handleWanData(wanData)
-        }
-    }
-    return err
+	for {
+		select {
+		case tunnelData := <-ct:
+			err = s.handleTunnelData(tunnelData)
+		case wanData := <-cw:
+			err = s.handleWanData(wanData)
+		}
+	}
+	return err
 }
 
 //Destroy vpn server instance.
 func (s *TinyServer) Close() error {
-    return nil
+	return nil
 }
 
-func StartUDPServer(serverAddr string, c chan[]byte) error {
-     listenAddr, err := net.ResolveUDPAddr("udp", serverAddr)
-     if err != nil {
-         fmt.Println("Error when resoving UDP Address!")
-         return err
-     }
+func StartUDPServer(serverAddr string, c chan []byte) error {
+	listenAddr, err := net.ResolveUDPAddr("udp", serverAddr)
+	if err != nil {
+		fmt.Println("Error when resoving UDP Address!")
+		return err
+	}
 
-     conn, err := net.ListenUDP("udp", listenAddr)
-     if err != nil {
-         fmt.Println("Error when listening to UDP Address!")
-         return err
-     }
-     defer conn.Close()
+	conn, err := net.ListenUDP("udp", listenAddr)
+	if err != nil {
+		fmt.Println("Error when listening to UDP Address!")
+		return err
+	}
+	defer conn.Close()
 
-     buf := make([]byte, BUFFERSIZE)
-     for {
-         _, addr, err := conn.ReadFromUDP(buf)
-         if err != nil {
-             fmt.Println("Error:reading from ", err, addr)
-         } else {
-             c <- buf
-         }
-     }
+	buf := make([]byte, BUFFERSIZE)
+	for {
+		_, addr, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			fmt.Println("Error:reading from ", err, addr)
+		} else {
+			c <- buf
+		}
+	}
 
-     return err
+	return err
 }
 
-func StartTCPServer(serverAddr string, c chan[]byte) error {
-     listenAddr, err := net.ResolveTCPAddr("tcp", serverAddr)
-     if err != nil {
-         fmt.Println("Error when resoving TCP Address!")
-         return err
-     }
+func StartTCPServer(serverAddr string, c chan []byte) error {
+	listenAddr, err := net.ResolveTCPAddr("tcp", serverAddr)
+	if err != nil {
+		fmt.Println("Error when resoving TCP Address!")
+		return err
+	}
 
-     ln, err := net.ListenTCP("tcp", listenAddr)
-     if err != nil {
-         fmt.Println("Error when listening to TCP Address!")
-         return err
-     }
-     defer ln.Close()
+	ln, err := net.ListenTCP("tcp", listenAddr)
+	if err != nil {
+		fmt.Println("Error when listening to TCP Address!")
+		return err
+	}
+	defer ln.Close()
 
-     for {
-         conn, err := ln.AcceptTCP()
-         if err != nil {
-             fmt.Println("Error: ", err)
-         } else {
-             go handleTCPConn(conn, c)
-         }
-    }
-    return err
+	for {
+		conn, err := ln.AcceptTCP()
+		if err != nil {
+			fmt.Println("Error: ", err)
+		} else {
+			go handleTCPConn(conn, c)
+		}
+	}
+	return err
 }
 
-func handleTCPConn(conn *net.TCPConn, c chan[]byte) {
-    buf := make([]byte, BUFFERSIZE)
+func handleTCPConn(conn *net.TCPConn, c chan []byte) {
+	buf := make([]byte, BUFFERSIZE)
 
-    _, err := conn.Read(buf)
-    //clentRecord[waterUtil.IPv4.String()] = conn
+	_, err := conn.Read(buf)
+	//clentRecord[waterUtil.IPv4.String()] = conn
 
-    if err != nil {
-        fmt.Println("Error:reading ", err)
-    } else {
-        c <- buf
-    }
+	if err != nil {
+		fmt.Println("Error:reading ", err)
+	} else {
+		c <- buf
+	}
 }
 
-func (s *TinyServer)handleWanData(buf []byte) error{
-   var err error
-   //TODO: Decrypt buf using pre-shared key
-   dst := waterutil.IPv4Destination(buf)
-   //src := waterutil.IPv4Source(buf)
-   if client := s.clientRecord[dst.String()]; client != nil {
-       _, err = client.Write(buf)
-   } else {
-       //TODO: decrypt the packet
-       _, err = s.tunnel.Write(buf)
-   }
-   return err
+func (s *TinyServer) handleWanData(buf []byte) (err error) {
+	//TODO: decrypt the packet
+	_, err = s.tunnel.Write(buf)
+	return
 }
 
-func (s *TinyServer)handleTunnelData(buf []byte) error {
-    var err error
-    dst := waterutil.IPv4Destination(buf)
-    if client := s.clientRecord[dst.String()]; client != nil {
-        _, err = client.Write(buf)
-        if err != nil {
-            fmt.Println("Failed to handle incomming tun data!")
-        }
-    }
-    return err
+func (s *TinyServer) handleTunnelData(buf []byte) error {
+	var err error
+	dst := waterutil.IPv4Destination(buf)
+	if client := s.clientRecord[dst.String()]; client != nil {
+		_, err = client.Write(buf)
+		if err != nil {
+			fmt.Println("Failed to handle incomming tun data!")
+		}
+	}
+	return err
 }
