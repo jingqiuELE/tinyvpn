671ff7fefc7c19cc1809b3cf0f649fd060c42d7d
jingqiuELE <aqiu0720@gmail.com>
2016-05-14 11:29:24 +0800

    A dummy implementation of auth module.

---
+++ petrel/auth.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+)
+
+func startAuthenticationServer(serverIP string, port int, s *SessionMap) (err error) {
+	serverAddr := serverIP + ":" + strconv.Itoa(port)
+	listenAddr, err := net.ResolveTCPAddr("tcp", serverAddr)
+	if err != nil {
+		fmt.Println("Error when resoving TCP Address!")
+		return err
+	}
+	l, err := net.ListenTCP("tcp", listenAddr)
+	if err != nil {
+		return
+	}
+	defer l.Close()
+	for {
+		conn, err := l.AcceptTCP()
+		if err != nil {
+			fmt.Println("Error: ", err)
+		} else {
+			go handleAuthConn(conn, s)
+		}
+	}
+	return
+}
+
+func handleAuthConn(conn *net.TCPConn, s *SessionMap) {
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Failed to read:", err)
+		return
+	}
+	sk, err := NewSessionKey()
+	if err != nil {
+		fmt.Println("Failed to create new SessionKey:", err)
+		return
+	}
+	s.Update(*sk, TConnection{})
+	buf = sk[:]
+	_, err = conn.Write(buf)
+	if err != nil {
+		fmt.Println("Failed to write SessionKey:", err)
+		return
+	}
+
+}
