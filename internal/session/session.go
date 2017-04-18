6219353d2ca8389b303e77b14d2c897a1a4afe50
Edward Jiang <edwardrf@gmail.com>
2017-04-18 12:02:08 +0800

    Remove moved files

--- internal/session/session.go
+++
@@ -1,9 +0,0 @@
-//session includes the type definition for a client's session.
-//A session has an index, which maps to a randomly generated session secret.
-package session
-
-const IndexLen = 6
-const SecretLen = 32
-
-type Index [IndexLen]byte
-type Secret [SecretLen]byte
