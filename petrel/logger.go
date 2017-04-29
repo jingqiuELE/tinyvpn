2a9e7b63638d25d2db4353d4305203510cb87313
Jing Qiu <aqiu0720@gmail.com>
2017-04-29 23:12:18 +0800

    Change logging format.

--- petrel/logger.go
+++ petrel/logger.go
@@ -9,7 +9,7 @@ import (
 var Logger = logging.MustGetLogger("petrel")
 
 func GetLogger(level logging.Level) *logging.Logger {
-	fmt_string := "\r%{color}[%{time:06-01-02 15:04:05}][%{level:.6s}]%{color:reset} %{message}"
+	fmt_string := "\r%{color}[%{level:.6s}]%{shortfunc}:%{color:reset} %{message}"
 	format := logging.MustStringFormatter(fmt_string)
 	backend := logging.NewLogBackend(os.Stdout, "", 0)
 	backendleveled := logging.AddModuleLevel(backend)
